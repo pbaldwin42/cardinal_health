15a374ed3b1f0b2e3bdb53dc5e53b3a66e2a4d33
Patrick Baldwin <Baldwin90@gmail.com>
2019-03-04 02:50:19 -0500

    base code

---
+++ src/com/baldwin/patrick/java_audition/Main.java
@@ -0,0 +1,9 @@
+package com.baldwin.patrick.java_audition;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        VendingMachine.run();
+        VendingMachineTests.run();
+    }
+}
---
+++ src/com/baldwin/patrick/java_audition/VendingMachineTests.java
@@ -0,0 +1,7 @@
+package com.baldwin.patrick.java_audition;
+
+public interface VendingMachineTests {
+    static void run() {
+        System.out.println("testing");
+    }
+}
